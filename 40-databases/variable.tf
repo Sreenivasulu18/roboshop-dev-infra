94811a9fc9b7bf73a5ec091388d7ab884aac29ea
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-12-11 23:20:19 +0530

    terraform

--- 40-databases/variable.tf
+++ 40-databases/variable.tf
@@ -6,10 +6,10 @@ variable "environment"{
 	default = "dev"
 }
 
-# variable "zone_id" {
-#     default = "Z010021512LXKVZRADNTG"
-# }
+variable "zone_id" {
+    default = "Z010021512LXKVZRADNTG"
+}
 
-# variable "domain_name" {
-#     default = "daws96s.fun"
-# }
+variable "domain_name" {
+    default = "daws96s.fun"
+}
