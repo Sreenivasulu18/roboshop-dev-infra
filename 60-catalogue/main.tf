59d54a83514bd511ad662ae10ef7a3547916b718
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-12-17 22:36:46 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -90,6 +90,9 @@ resource "aws_launch_template" "catalogue" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.catalogue_sg_id]
 
+  # when we run terraform apply again, a new version will be created with new AMI ID
+  update_default_version = true
+
   # tags attched to the instances	
    tag_specifications {
         resource_type = "instance"
@@ -134,6 +137,15 @@ resource "aws_autoscaling_group" "catalogue" {
   }
   vpc_zone_identifier       = local.private_subnet_ids
   target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50 # atleast 50% of the instance should be up and running
+    }
+    triggers = ["launch_template"]
+  }
+  
   dynamic "tag" { # we will get the iterator with name as tag
 	for_each =  merge(
 		local.common_tags,
