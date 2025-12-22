6987db57c511210a3630c59a9d1d887bab7140ce
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-12-22 15:00:35 +0530

    terraform

--- 90-components/provider.tf
+++ 90-components/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "remote-state-96s-123-dev"
-    key    = "roboshop-dev-component"
+    key    = "roboshop-dev-components"
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
