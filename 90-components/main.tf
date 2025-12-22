fadd44a25c7d1e6067eded3241d098532494e0c3
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-12-22 16:14:45 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,12 +1,12 @@
-module "component"{
-    source = "../../terraform-roboshop-component"
-    component = var.component
-    rule_priority = var.rule_priority
-}
-
-# module "component"{
-# 	for_each = var.component
-# 	source = "git::?ref=main"
-# 	component = eack.key
-# 	rule_ptiority = each.value. rule_priority
+# module "components"{
+#     source = "../../terraform-roboshop-component"
+#     component = var.component
+#     rule_priority = var.rule_priority
 # }
+
+module "components"{
+	for_each = var.components
+	source = "git::https://github.com/Sreenivasulu18/terraform-roboshop-component.git?ref=main"
+	component = eack.key
+	rule_ptiority = each.value.rule_priority
+}
--- 90-components/variable.tf
+++ 90-components/variable.tf
@@ -6,25 +6,25 @@ variable "rule_priority" {
     default = 10
 }
 
-# variable "component"{
-#     default = {
-#         catalogue = {
-#             default = 10
-#         }
-#         user = {
-#             default = 20
-#         }
-#         cart = {
-#             default = 30
-#         }
-#         shipping = {
-#             default = 40
-#         }
-#         payment = {
-#             default = 20
-#         }
-#         frontend = {
-#             default = 10
-#         }
-#     }
-# }
+variable "components"{
+    default = {
+        catalogue = {
+            default = 10
+        }
+        user = {
+            default = 20
+        }
+        cart = {
+            default = 30
+        }
+        shipping = {
+            default = 40
+        }
+        payment = {
+            default = 20
+        }
+        frontend = {
+            default = 10
+        }
+    }
+}
