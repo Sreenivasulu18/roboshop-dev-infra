e4ae37bc396c64035c7e8b4d11f2e46452de0bae
Sreenivasulu18 <singalasreenivasulu2@gmail.com>
2025-12-22 13:59:26 +0530

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -8,3 +8,10 @@ xfs_growfs /home
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
+
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/Sreenivasulu18/roboshop-dev-infra.git
+cd roboshop-dev-infra/40-databases
+terraform init
+terraform apply -auto-approve
